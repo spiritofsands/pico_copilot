be570208e157a0ae0dab45603f12e616e943ea86
spiritofsands <spiritofsands@protonmail.com>
2025-01-11 19:00:44 +0200

    Redraw test GUI

--- tests/integration/emulator.py
+++ tests/integration/emulator.py
@@ -1,7 +1,14 @@
 """Main entrance point."""
 
-from tkinter import (
-    Tk, Button, Canvas, Frame, Label, Scale, StringVar, LEFT, VERTICAL)
+from tkinter import (Tk,
+                     Button,
+                     Canvas,
+                     Frame,
+                     Label,
+                     Scale,
+                     StringVar,
+                     LEFT,
+                     VERTICAL)
 from colorsys import hsv_to_rgb
 
 from pico_copilot.utils.logger import LOG
@@ -31,14 +38,14 @@ class Emulator:
         self._tail_light_frame = None
         self._front_light_frame = None
 
-        self._window_width = 1200
+        self._window_width = 800
         self._window_height = 300
-        self._bar_width = 50
-        self._bar_height = 50
+        self._bar_width = 30
+        self._bar_height = self._window_height
 
         self._tail_light_width = self._window_width // 3
         self._tail_light_height = self._window_height
-        self._front_light_width = 2 * self._window_width // 3
+        self._front_light_width = self._window_width // 3
         self._front_light_height = self._window_height
 
         self._status_light_width = self._window_width // 6
@@ -68,7 +75,6 @@ class Emulator:
                 },
                 'updated': True,
                 'color': self.YELLOW_HSV_COLOR,
-
             },
             'status': {
                 'leds': {
@@ -147,75 +153,67 @@ class Emulator:
 
     def _create_elements(self):
         """Create all elements like led stripes, buttons, etc."""
-        self._tail_light_frame = Frame(
-            master=self._root,
-            width=self._window_width // 2,
-            height=self._window_height,
-            bg='grey')
+        self._tail_light_frame = Frame(master=self._root,
+                                       width=self._window_width // 2,
+                                       height=self._window_height,
+                                       bg='grey')
         self._tail_light_frame.pack(padx=30, side=LEFT)
         self._add_label(self._tail_light_frame, 'Tail light')
-        self._tail_light_canvas = Canvas(
-            self._tail_light_frame, width=self._tail_light_width,
-            height=self._tail_light_height)
+        self._tail_light_canvas = Canvas(self._tail_light_frame,
+                                         width=self._tail_light_width,
+                                         height=self._tail_light_height)
         self._tail_light_canvas.pack()
 
-        self._front_light_frame = Frame(
-            master=self._root,
-            width=self._window_width // 2,
-            height=self._window_height,
-            bg='grey')
+        self._front_light_frame = Frame(master=self._root,
+                                        width=self._window_width // 2,
+                                        height=self._window_height,
+                                        bg='grey')
         self._front_light_frame.pack(padx=30, side=LEFT)
         self._add_label(self._front_light_frame, 'Front light')
-        self._front_light_canvas = Canvas(
-            self._front_light_frame,
-            width=self._front_light_width,
-            height=self._front_light_height)
+        self._front_light_canvas = Canvas(self._front_light_frame,
+                                          width=self._front_light_width,
+                                          height=self._front_light_height)
         self._front_light_canvas.pack()
 
-        self._status_light_frame = Frame(
-            master=self._root,
-            width=self._window_width // 2,
-            height=self._window_height,
-            bg='grey')
+        self._status_light_frame = Frame(master=self._root,
+                                         width=self._window_width // 2,
+                                         height=self._window_height,
+                                         bg='grey')
         self._status_light_frame.pack(padx=30, side=LEFT)
         self._add_label(self._status_light_frame, 'Status light')
-        self._status_light_canvas = Canvas(
-            self._status_light_frame,
-            width=self._status_light_width,
-            height=self._status_light_height)
+        self._status_light_canvas = Canvas(self._status_light_frame,
+                                           width=self._status_light_width,
+                                           height=self._status_light_height)
         self._status_light_canvas.pack()
 
-        self._light_sensor_frame = Frame(
-            master=self._root,
-            width=self._window_width // 2,
-            height=self._window_height,
-            bg='grey')
+        self._light_sensor_frame = Frame(master=self._root,
+                                         width=self._window_width // 2,
+                                         height=self._window_height,
+                                         bg='grey')
         self._light_sensor_frame.pack(padx=30, side=LEFT)
         self._add_label(self._light_sensor_frame, 'Light sensor')
 
         self._light_scale_value = StringVar()
-        self._light_sensor_scale = Scale(
-            self._light_sensor_frame,
-            variable=self._light_scale_value,
-            from_=self.MAX_BRIGHTNESS, to=0.0,
-            digits=2,
-            resolution=0.1,
-            orient=VERTICAL)
+        self._light_sensor_scale = Scale(self._light_sensor_frame,
+                                         variable=self._light_scale_value,
+                                         from_=self.MAX_BRIGHTNESS,
+                                         to=0.0,
+                                         digits=2,
+                                         resolution=0.1,
+                                         orient=VERTICAL)
         self._light_sensor_scale.set(self.DEFAULT_BRIGHTNESS)
         self._light_sensor_scale.pack()
 
-        self._button_frame = Frame(
-            master=self._root,
-            width=self._window_width // 2,
-            height=self._window_height,
-            bg='grey')
+        self._button_frame = Frame(master=self._root,
+                                   width=self._window_width // 2,
+                                   height=self._window_height,
+                                   bg='grey')
         self._button_frame.pack(padx=30, side=LEFT)
         self._add_label(self._button_frame, 'Button')
-        self._button_element = Button(
-            self._button_frame,
-            width=self._button_width,
-            height=self._button_height,
-            bg='grey')
+        self._button_element = Button(self._button_frame,
+                                      width=self._button_width,
+                                      height=self._button_height,
+                                      bg='grey')
         self._button_element.bind("<ButtonPress>", self._button_click)
         self._button_element.bind("<ButtonRelease>", self._button_release)
         self._button_element.pack()
@@ -224,155 +222,61 @@ class Emulator:
         label = Label(parent, text=text)
         label.pack(fill='both')
 
+    def _get_bar_rectangle(self, start_x, start_y):
+        return (start_x,
+                start_y,
+                start_x + self._bar_width,
+                start_y + self._bar_height)
+
     def _draw_front_light(self):
-        # bars
-        self._front_light_canvas.create_polygon(
-            self._front_light_width // 2 - self._bar_width,
-            self._front_light_height // 2,
-            self._front_light_width // 2 - self._bar_width,
-            self._front_light_height,
-            self._front_light_width // 2, self._front_light_height,
-            self._front_light_width // 2, self._front_light_height // 2,
-            outline='black',
-            fill=self._colors['front']['leds']['front_bars'])
-        self._front_light_canvas.create_polygon(
-            self._front_light_width // 2 + self._bar_width,
-            self._front_light_height // 2,
-            self._front_light_width // 2 + self._bar_width,
-            self._front_light_height,
-            self._front_light_width // 2, self._front_light_height,
-            self._front_light_width // 2, self._front_light_height // 2,
+        self._front_light_canvas.create_rectangle(
+            *self._get_bar_rectangle(0,
+                                     0),
             outline='black',
             fill=self._colors['front']['leds']['front_bars'])
 
-        # wings
-        segment_width = (self._front_light_width // 2 - self._bar_width) // 3
-
-        self._front_light_canvas.create_polygon(
-            0, self._front_light_height - 5 * self._bar_height // 2,
-            0, self._front_light_height - 3 * self._bar_height // 2,
-            segment_width, self._front_light_height - self._bar_height,
-            segment_width, self._front_light_height - self._bar_height * 2,
+        self._front_light_canvas.create_rectangle(
+            *self._get_bar_rectangle(self._bar_width * 2,
+                                     0),
             outline='black',
             fill=self._colors['front']['leds']['segment_edge'])
-        self._front_light_canvas.create_polygon(
-            segment_width, self._front_light_height - self._bar_height * 2,
-            segment_width, self._front_light_height - self._bar_height,
-            segment_width * 2,
-            self._front_light_height - self._bar_height // 2,
-            segment_width * 2,
-            self._front_light_height - 3 * self._bar_height // 2,
-            outline='black',
-            fill=self._colors['front']['leds']['segment_mid'])
-        self._front_light_canvas.create_polygon(
-            segment_width * 2,
-            self._front_light_height - 3 * self._bar_height // 2,
-            segment_width * 2,
-            self._front_light_height - self._bar_height // 2,
-            segment_width * 3, self._front_light_height,
-            segment_width * 3, self._front_light_height - self._bar_height,
-            outline='black',
-            fill=self._colors['front']['leds']['segment_center'])
 
-        self._front_light_canvas.create_polygon(
-            self._front_light_width,
-            self._front_light_height - 5 * self._bar_height // 2,
-            self._front_light_width,
-            self._front_light_height - 3 * self._bar_height // 2,
-            self._front_light_width - segment_width,
-            self._front_light_height - self._bar_height,
-            self._front_light_width - segment_width,
-            self._front_light_height - self._bar_height * 2,
-            outline='black',
-            fill=self._colors['front']['leds']['segment_edge'])
-        self._front_light_canvas.create_polygon(
-            self._front_light_width - segment_width,
-            self._front_light_height - self._bar_height * 2,
-            self._front_light_width - segment_width,
-            self._front_light_height - self._bar_height,
-            self._front_light_width - segment_width * 2,
-            self._front_light_height - self._bar_height // 2,
-            self._front_light_width - segment_width * 2,
-            self._front_light_height - 3 * self._bar_height // 2,
+        self._front_light_canvas.create_rectangle(
+            *self._get_bar_rectangle(self._bar_width * 4,
+                                     0),
             outline='black',
             fill=self._colors['front']['leds']['segment_mid'])
-        self._front_light_canvas.create_polygon(
-            self._front_light_width - segment_width * 2,
-            self._front_light_height - 3 * self._bar_height // 2,
-            self._front_light_width - segment_width * 2,
-            self._front_light_height - self._bar_height // 2,
-            self._front_light_width - segment_width * 3,
-            self._front_light_height,
-            self._front_light_width - segment_width * 3,
-            self._front_light_height - self._bar_height,
+
+        self._front_light_canvas.create_rectangle(
+            *self._get_bar_rectangle(self._bar_width * 6,
+                                     0),
             outline='black',
             fill=self._colors['front']['leds']['segment_center'])
 
     def _draw_tail_light(self):
-        self._bar_width = self._tail_light_width // 8
-        self._bar_height = self._tail_light_height // 8
-        x_margin = self._bar_width
-        y_margin = self._bar_height
-
-        # bars
         self._tail_light_canvas.create_rectangle(
-            0, 0,
-            self._bar_width, self._tail_light_height,
+            *self._get_bar_rectangle(0,
+                                     0),
             outline='black',
             fill=self._colors['tail']['leds']['tail_bars'])
 
-        right_bar_x = self._tail_light_width - self._bar_width
         self._tail_light_canvas.create_rectangle(
-            right_bar_x, 0,
-            right_bar_x + self._bar_width,
-            self._tail_light_height,
+            *self._get_bar_rectangle(self._bar_width * 2,
+                                     0),
             outline='black',
-            fill=self._colors['tail']['leds']['tail_bars'])
+            fill=self._colors['tail']['leds']['top_v'])
 
-        def v_coords(x_coord, y_coord, v_width, v_height):
-            """Return the coords of "V"."""
-            return (x_coord, y_coord,
-                    x_coord, y_coord + v_height // 2,
-                    x_coord + v_width // 2, y_coord + v_height,
-                    x_coord + v_width, y_coord + v_height // 2,
-                    x_coord + v_width, y_coord,
-                    x_coord + v_width // 2, y_coord + v_height // 2)
+        self._tail_light_canvas.create_rectangle(
+            *self._get_bar_rectangle(self._bar_width * 4,
+                                     0),
+            outline='black',
+            fill=self._colors['tail']['leds']['mid_v'])
 
-        def inverted_v_coords(x_coord, y_coord, v_width, v_height):
-            """Return the coords of "^"."""
-            return (x_coord, y_coord + v_height,
-                    x_coord, y_coord + v_height // 2,
-                    x_coord + v_width // 2, y_coord,
-                    x_coord + v_width, y_coord + v_height // 2,
-                    x_coord + v_width, y_coord + v_height,
-                    x_coord + v_width // 2, y_coord + v_height // 2)
-
-        v_bar_x = self._bar_width + x_margin
-
-        self._tail_light_canvas.create_polygon(
-            *v_coords(
-                v_bar_x, (self._bar_height + y_margin) * 0,
-                self._tail_light_width - (self._bar_width + x_margin) * 2,
-                self._bar_height + y_margin),
-            outline='black', fill=self._colors['tail']['leds']['top_v'])
-        self._tail_light_canvas.create_polygon(
-            *v_coords(
-                v_bar_x, (self._bar_height + y_margin) * 1,
-                self._tail_light_width - (self._bar_width + x_margin) * 2,
-                self._bar_height + y_margin),
-            outline='black', fill=self._colors['tail']['leds']['mid_v'])
-        self._tail_light_canvas.create_polygon(
-            *v_coords(
-                v_bar_x, (self._bar_height + y_margin) * 2,
-                self._tail_light_width - (self._bar_width + x_margin) * 2,
-                self._bar_height + y_margin),
-            outline='black', fill=self._colors['tail']['leds']['low_x'])
-        self._tail_light_canvas.create_polygon(
-            *inverted_v_coords(
-                v_bar_x, self._bar_height * 6,
-                self._tail_light_width - (self._bar_width + x_margin) * 2,
-                self._bar_height + y_margin),
-            outline='black', fill=self._colors['tail']['leds']['low_x'])
+        self._tail_light_canvas.create_rectangle(
+            *self._get_bar_rectangle(self._bar_width * 6,
+                                     0),
+            outline='black',
+            fill=self._colors['tail']['leds']['low_x'])
 
     def _draw_status_light(self):
         """Draw a status LED."""
@@ -381,9 +285,12 @@ class Emulator:
         led_y = self._status_light_height // 2
 
         self._status_light_canvas.create_oval(
-            led_x - led_radius, led_y - led_radius,
-            led_x + led_radius, led_y + led_radius,
-            outline='black', fill=self._colors['status']['leds']['status'])
+            led_x - led_radius,
+            led_y - led_radius,
+            led_x + led_radius,
+            led_y + led_radius,
+            outline='black',
+            fill=self._colors['status']['leds']['status'])
 
 
 class BoardInterface:
@@ -395,8 +302,7 @@ class BoardInterface:
         self._board = board
         for _set_name, set_params in config['leds'].items():
             for name, params in set_params['leds'].items():
-                self._board.set_led_brightness(
-                    name, self.DEFAULT_BRIGHTNESS)
+                self._board.set_led_brightness(name, self.DEFAULT_BRIGHTNESS)
 
     def change_brightness(self, name, brightness):
         """Change brightness for a led element in the window."""
